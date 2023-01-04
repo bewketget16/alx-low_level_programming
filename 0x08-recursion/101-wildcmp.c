1bf1e5b8f20e74e5acad1fe000095ba72e206652
Bewket <brishget58@gmail.com>
2023-01-04 10:22:28 -0800

    Compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,6 +5,7 @@
  * @s1: pointer to string params
  * @s2: pointer to string params
  * Return: 0
+ *
  */
 
 int wildcmp(char *s1, char *s2)
@@ -17,4 +18,15 @@ int wildcmp(char *s1, char *s2)
 		}
 		return (*s2 == '\0');
 	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
 
